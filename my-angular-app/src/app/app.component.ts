0b31f281172c7768845dfc61f5879d7174f9ff89
seriouslucy <seriouslucy@outlook.com>
2025-03-18 19:06:08 -0500

    modified
    deleted template, created new components with styling

--- my-angular-app/src/app/app.component.ts
+++ my-angular-app/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from './footer/footer.component';
+import { DisplayComponent } from "./display/display.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, DisplayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
